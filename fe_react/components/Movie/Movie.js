842906928384b99ead889b8a4c83ba3145915756
Your Namemovorpovor <movorpovor@yandex.ru>
2017-11-14 01:39:44 +0300

    Revert "this is work commit"
    This reverts commit 64b17209098fd2aeb07f2b1cff3fd900c1f2a959.

--- fe_react/components/Movie/Movie.js
+++ fe_react/components/Movie/Movie.js
@@ -1,55 +1,23 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import { DragSource } from 'react-dnd';
+import React, {PropTypes, Component} from 'react'
 
-const cardSource = {
-    beginDrag(props) {
-    return {
-            
-        };
-    }
-};
-
-function collect(connect, monitor) {
-    return {
-        connectDragSource: connect.dragSource(),
-        isDragging: monitor.isDragging()
-    };
-}
-
-const propTypes = {
-    movie: PropTypes.object.isRequired,
-  
-    // Injected by React DnD:
-    isDragging: PropTypes.bool.isRequired,
-    connectDragSource: PropTypes.func.isRequired
-};
-
-class Movie extends Component{
+export default class Movie extends Component{
     constructor(props){
         super(props);
-
-        this.state = {
-            dragging: false
-        };
     }   
 
     render() {
-        const { isDragging, connectDragSource, movie } = this.props;
-        return connectDragSource(
+        var movie = this.props.movie;
+        return (
           <div className='movie'>
-            <img src={movie.cover} className='cover'/>
-            <div className='movie-info'>
+              <img src={movie.cover} className='cover'/>
+              <div className='movie-info'>
                 <div className='titles'>
                     <a href={movie.km_ref} target="_blank">{movie.title}</a>
                     <a className='original-title'>{movie.original_title}</a>
                 </div>
                 <p className='about-movie'>{movie.description}</p>
-            </div>
+              </div>
           </div>
-        );
+        )
     }
 }
-Movie.propTypes = propTypes;
-
-export default DragSource('movie', cardSource, collect)(Movie);
