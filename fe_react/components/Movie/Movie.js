c724ca21330c64fbe0dce101b88c142372c46141
movorpovor <movorpovor@yandex.ru>
2017-11-23 12:26:43 +0300

    fix bug with tiny cover

--- fe_react/components/Movie/Movie.js
+++ fe_react/components/Movie/Movie.js
@@ -1,28 +1,8 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import { DragSource } from 'react-dnd';
-
-const cardSource = {
-    beginDrag(props) {
-    return {
-            
-        };
-    }
-};
-
-function collect(connect, monitor) {
-    return {
-        connectDragSource: connect.dragSource(),
-        isDragging: monitor.isDragging()
-    };
-}
 
 const propTypes = {
     movie: PropTypes.object.isRequired,
-  
-    // Injected by React DnD:
-    isDragging: PropTypes.bool.isRequired,
-    connectDragSource: PropTypes.func.isRequired
 };
 
 class Movie extends Component{
@@ -36,20 +16,20 @@ class Movie extends Component{
 
     render() {
         const { isDragging, connectDragSource, movie } = this.props;
-        return connectDragSource(
-          <div className='movie'>
-            <img src={movie.cover} className='cover'/>
-            <div className='movie-info'>
-                <div className='titles'>
-                    <a href={movie.km_ref} target="_blank">{movie.title}</a>
-                    <a className='original-title'>{movie.original_title}</a>
+        return(
+            <div className='movie'>
+                <img className='cover' src={movie.cover} />
+                <div className='movie-info'>
+                    <div className='titles'>
+                        <a href={movie.km_ref} target="_blank">{movie.title}</a>
+                        <a className='original-title'>{movie.original_title}</a>
+                    </div>
+                    <p className='about-movie'>{movie.description}</p>
                 </div>
-                <p className='about-movie'>{movie.description}</p>
             </div>
-          </div>
         );
     }
 }
 Movie.propTypes = propTypes;
 
-export default DragSource('movie', cardSource, collect)(Movie);
+export default Movie;
