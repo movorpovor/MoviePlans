3c8e3e7e01bd1aae24e69073613043a1d36ee92a
movorpovor <movorpovor@yandex.ru>
2017-11-24 23:00:34 +0300

    the end of merge

--- fe_react/components/Movie/Movie.js
+++ fe_react/components/Movie/Movie.js
@@ -3,33 +3,6 @@ import PropTypes from 'prop-types'
 
 const propTypes = {
     movie: PropTypes.object.isRequired,
-<<<<<<< HEAD
-};
-
-class Movie extends Component{
-    constructor(props){
-        super(props);
-    }   
-
-    render() {
-        var movie = this.props.movie;
-        return (
-          <div className='movie'>
-              <img src={movie.cover} className='cover'/>
-              <div className='movie-info'>
-                <div className='titles'>
-                    <a href={movie.km_ref} target="_blank">{movie.title}</a>
-                    <a className='original-title'>{movie.original_title}</a>
-                </div>
-                <p className='about-movie'>{movie.description}</p>
-              </div>
-          </div>
-        )
-    }
-}
-
-export default Movie
-=======
     isDragging: PropTypes.bool,
     isDragPreview: PropTypes.bool
 };
@@ -58,4 +31,3 @@ const Movie = (props) => {
 Movie.propTypes = propTypes;
 
 export default Movie;
->>>>>>> dnd
